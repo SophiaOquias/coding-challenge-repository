ddd54ad78a4fcd6d8b39cf7b1f5b67711eb1a34c
Sophia <sophia.oquias@gmail.com>
2024-09-25 20:00:37 +0800

    implemented isValid function

--- problem_set_2/valid_parentheses.go
+++ problem_set_2/valid_parentheses.go
@@ -31,12 +31,57 @@ func (stack Stack) print() {
 
 }
 
-func main() {
+func (stack Stack) isEmpty() bool {
+	if(stack.top == nil) {
+		return true
+	}
+
+	return false
+}
+
+func isOpenBracket(bracket rune) bool {
+	if bracket == '(' || bracket == '[' || bracket == '{' {
+		return true
+	}
+
+	return false 
+}
+
+func getMatch(bracket rune) rune {
+	switch bracket {
+		case ')': return '(' 
+		case ']': return '['
+		case '}': return '{'
+		default: return 0 
+	}
+}
+
+func isValid(s string) bool {
 	stack := &Stack{}
 
-	stack.push('A')
-	stack.push('B')
-	stack.push('C')
+	for i := 0; i < len(s); i++ {
+		current := rune(s[i])
 
-	stack.print()
+		if isOpenBracket(current) {
+			stack.push(current)
+		} else {
+			if stack.isEmpty() {
+				return false 
+			} 
+
+			if getMatch(current) != stack.top.item {
+				return false
+			}
+
+			stack.pop()
+		}
+	}
+
+	return true
+}
+
+func main() {
+	inputString := "([)]"
+	result := isValid(inputString)
+	fmt.Println(result) 
 }
