2b1b9d479431ed5d9c87b616f4c342f566267fea
Sophia <sophia.oquias@gmail.com>
2024-09-25 15:02:46 +0800

    added code template

---
+++ problem_set_1/palindrome_pairs.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func isPalindrome(s string) bool {
+
+}
+
+func palindromePairs(words []string) [][]int {
+
+}
+
+func main() {
+	inputWords := []string{"bat", "tab", "cat"}
+	result := palindromePairs(inputWords)
+	fmt.Println(result) // Output: [[0 1] [1 0]]
+}
