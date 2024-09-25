3356a0f0d65e2b1dc1d8a845201f4253d297640f
Sophia <sophia.oquias@gmail.com>
2024-09-25 15:35:18 +0800

    created function to reverse string

--- problem_set_1/palindrome_pairs.go
+++ problem_set_1/palindrome_pairs.go
@@ -2,16 +2,31 @@ package main
 
 import "fmt"
 
-func isPalindrome(s string) bool {
+func reverseString(s string) string {
+	var reverse = ""
+	var start = len(s) - 1
 
+	for i := start; i >= 0; i-- {
+		reverse = reverse + string(s[i])
+	}
+
+	return reverse; 
 }
 
-func palindromePairs(words []string) [][]int {
+// func isPalindrome(s string) bool {
 
-}
+// }
+
+// func palindromePairs(words []string) [][]int {
+
+// }
 
 func main() {
-	inputWords := []string{"bat", "tab", "cat"}
-	result := palindromePairs(inputWords)
-	fmt.Println(result) // Output: [[0 1] [1 0]]
+	// inputWords := []string{"bat", "tab", "cat"}
+	// result := palindromePairs(inputWords)
+	// fmt.Println(result) // Output: [[0 1] [1 0]]
+
+	reverse := reverseString("hello")
+
+	fmt.Println(reverse)
 }
