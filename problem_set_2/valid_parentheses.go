cd22c5e579379d07b96b6fb2885c84abb0ed9eda
Sophia <sophia.oquias@gmail.com>
2024-09-25 19:31:49 +0800

    implemented stack structure

--- problem_set_2/valid_parentheses.go
+++ problem_set_2/valid_parentheses.go
@@ -2,6 +2,41 @@ package main
 
 import "fmt"
 
+type Node struct {
+	item rune 
+	next *Node
+}
+
+type Stack struct {
+	top *Node
+}
+
+func (stack *Stack) push(bracket rune) {
+	node := &Node{item: bracket, next: stack.top}
+
+	stack.top = node
+}
+
+func (stack *Stack) pop() {
+	stack.top = stack.top.next
+}
+
+func (stack Stack) print() {
+	current := stack.top
+
+	for current != nil {
+		fmt.Printf("%c\n", current.item)
+		current = current.next
+	}
+
+}
+
 func main() {
-	fmt.Println("Hello world!")
+	stack := &Stack{}
+
+	stack.push('A')
+	stack.push('B')
+	stack.push('C')
+
+	stack.print()
 }
